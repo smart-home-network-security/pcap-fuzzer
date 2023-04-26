c9bcf605ea39d964701559c31fd133c3f4a995fa
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2023-04-26 16:44:44 +0200

    CI: run script on all pcaps

---
+++ .ci_scripts/run-all-pcaps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+EXITCODE=0
+
+for pcap in $GITHUB_WORKSPACE/traces/*.pcap
+do
+    # Run pcap-tweaker script on pcap file
+    python3 $GITHUB_WORKSPACE/src/pcap-tweaker.py $pcap
+    # If the exit code is not 0, set EXITCODE to 1
+    if [[ $? -ne 0 ]]
+    then
+        EXITCODE=1
+    fi
+done
+
+exit $EXITCODE
