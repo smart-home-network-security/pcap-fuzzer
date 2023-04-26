6ca69434fcf4e01b3c25e32521cdbfd4666a2038
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2023-04-26 12:17:48 +0200

    Refactored DHCP

--- src/packet/DHCP.py
+++ src/packet/DHCP.py
@@ -1,5 +1,4 @@
 from typing import Tuple
-import re
 import random
 import scapy.all as scapy
 from scapy.layers import dhcp
@@ -11,10 +10,10 @@ class DHCP(Packet):
     name = "DHCP"
 
     # Modifiable fields
-    fields = {
-        "message-type": "int[1,8]",
-        "client_id": "str",  # Actually a byte array
-    }
+    fields = [
+        "message-type",
+        "client_id"
+    ]
 
 
     def get_dhcp_option(self, option_name) -> Tuple[str, any]:
@@ -50,7 +49,7 @@ class DHCP(Packet):
         Randomly edit one DHCP option.
         """
         # Get field which will be modified
-        field, value_type = random.choice(list(self.fields.items()))
+        field = random.choice(self.fields)
         # Store old value of field
         old_value = self.get_dhcp_option(field)[1]
 
@@ -58,35 +57,14 @@ class DHCP(Packet):
         new_value = old_value
         while new_value == old_value:
 
-            if isinstance(value_type, list):
-                # Field value is a list
-                # Choose randomly a value from the list
-                values = value_type
-                new_value = old_value
-                # Randomly pick new value
-                new_value = bytes(random.choice(values), "utf-8")
+            if field == "message-type":
+                # Message type is an integer between 1 and 8
+                new_value = random.randint(1, 8)
 
-            elif "int" in value_type:
-                # Field value is an integer
-                # Generate a random integer between given range
-                if value_type == "int":
-                    # No range given, default is 0-65535
-                    new_value = random.randint(0, 65535)
-                else:
-                    # Range given
-                    pattern = re.compile(r"int\[\s*(?P<start>\d+),\s*(?P<end>\d+)\s*\]")
-                    match = pattern.match(value_type)
-                    start = int(match.group("start"))
-                    end = int(match.group("end"))
-                    new_value = random.randint(start, end)
-
-            elif value_type == "str":
-                # Field value is a string
+            elif field == "client_id":
+                # Client ID is a byte array
                 # Randomly change one character
-                char = random.choice(Packet.ALPHANUM)
-                new_value = list(old_value)
-                new_value[random.randint(0, len(new_value) - 1)] = char
-                new_value = bytes(new_value)
+                new_value = Packet.string_edit_char(old_value)
 
         # Set new value for field
         print(f"Packet {self.id}: {self.name}.{field} = {old_value} -> {new_value}")
