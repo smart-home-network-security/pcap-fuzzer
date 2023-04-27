55ae83c52d9a4e46f7df5df876723ea2ee6edf39
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2023-04-27 15:32:43 +0200

    Put resulting CSV files in specific directory

--- src/pcap-tweaker.py
+++ src/pcap-tweaker.py
@@ -14,7 +14,10 @@ from packet.Packet import Packet
 
 if __name__ == "__main__":
 
+    # Script-related variables
     script_name = os.path.basename(__file__)
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    base_path = os.path.dirname(script_path)
 
     ### LOGGING CONFIGURATION ###
     logging.basicConfig(level=logging.INFO)
@@ -22,59 +25,59 @@ if __name__ == "__main__":
 
 
     ### ARGUMENT PARSING ###
-    
     parser = argparse.ArgumentParser(
         prog=script_name,
         description="Randomly edit packet fields in a PCAP file."
     )
-
-    # Positional argument #1: input PCAP file
-    parser.add_argument("input_pcap", type=str, help="Input PCAP file.")
-    # Optional argument -o: output PCAP file
-    parser.add_argument("-o", "--output_pcap", type=str, help="Output PCAP file. Default is input file name with '.tweak' appended.")
-
+    # Positional arguments: input PCAP file
+    parser.add_argument("input_pcaps", metavar="pcap", type=str, nargs="+", help="Input PCAP files.")
     args = parser.parse_args()
-    if args.output_pcap is None:
-        args.output_pcap = args.input_pcap.replace(".pcap", ".tweak.pcap")
 
     
     ### MAIN PROGRAM ###
 
-    # Read input PCAP file
-    packets = scapy.rdpcap(args.input_pcap)
-    logging.info(f"Read input PCAP file: {args.output_pcap}")
-    new_packets = []
+    # Loop on given input PCAP files
+    for input_pcap in args.input_pcaps:
+        # Read input PCAP file
+        packets = scapy.rdpcap(input_pcap)
+        logging.info(f"Read input PCAP file: {input_pcap}")
+        new_packets = []
 
-    # Open log CSV file
-    with open("tweaked_packets.csv", "w") as csv_file:
-        field_names = ["id", "protocol", "field", "old_value", "new_value"]
-        writer = csv.DictWriter(csv_file, fieldnames=field_names)
-        writer.writeheader()
+        # Open log CSV file
+        csv_dir = os.path.join(base_path, "csv")
+        os.makedirs(csv_dir, exist_ok=True)
+        csv_log = input_pcap.replace(".pcap", ".edit.csv")
+        csv_log = os.path.join(csv_dir, os.path.basename(csv_log))
+        with open(csv_log, "w") as csv_file:
+            field_names = ["id", "protocol", "field", "old_value", "new_value"]
+            writer = csv.DictWriter(csv_file, fieldnames=field_names)
+            writer.writeheader()
 
-        # Loop on packets
-        i = 0
-        for packet in packets:
+            # Loop on packets
+            i = 0
+            for packet in packets:
 
-            # Choose randomly if we edit this packet
-            #if random.randint(0, 1) != 0:
-            if i != 0:
-                # Packet won't be edited
-                # Go to next packet
-                new_packets.append(packet)
-                i += 1
-                continue
+                # Choose randomly if we edit this packet
+                #if random.randint(0, 1) != 0:
+                if i != 0:
+                    # Packet won't be edited
+                    # Go to next packet
+                    new_packets.append(packet)
+                    i += 1
+                    continue
 
-            # Edit packet, if possible
-            try:
-                packet = Packet.init_packet(packet.lastlayer().name, packet, i)
-                d = packet.tweak()
-                new_packets.append(packet.get_packet())
-                writer.writerow(d)
-            except ModuleNotFoundError:
-                new_packets.append(packet)
-            finally:
-                i += 1
+                # Edit packet, if possible
+                try:
+                    packet = Packet.init_packet(packet.lastlayer().name, packet, i)
+                    d = packet.tweak()
+                    new_packets.append(packet.get_packet())
+                    writer.writerow(d)
+                except ModuleNotFoundError:
+                    new_packets.append(packet)
+                finally:
+                    i += 1
 
-    # Write output PCAP file
-    #scapy.wrpcap(args.output_pcap, new_packets)
-    logging.info(f"Wrote output PCAP file: {args.output_pcap}")
+        # Write output PCAP file
+        output_pcap = input_pcap.replace(".pcap", ".edit.pcap")
+        #scapy.wrpcap(output_pcap, new_packets)
+        logging.info(f"Wrote output PCAP file: {output_pcap}")
